b7ffe3aecc026dd4fa97a90b7c64dd3fe7e1e5d5
RegalxCoding <rushabhtak07@gmail.com>
2025-06-18 18:35:42 +0530

    deleted all content from master

--- session/28Jan/fact.js
+++
@@ -1,6 +0,0 @@
-let a=6
-let fact=1
-for(let i=1;i<=a;i++){
-    fact=fact*i;
-}
-console.log(fact)
--- session/29Jan/Myscript.js
+++
@@ -1,9 +0,0 @@
-function hello(){
-    alert("hello, this is external JS");
-}
-function printDayOfWeek(){
-    const weekday=['sun','mon','tue','wednesday','thur','fri','sat'];
-    const date=new Date();
-    let day=date.getDay()
-    document.writeln("Today is "+weekday[day])
-}
