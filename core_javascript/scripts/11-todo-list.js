b258a9c14d0d2ea546f2b0e6e56ca3b7bfc69764
RegalxCoding <rushabhtak07@gmail.com>
2025-07-14 21:51:22 +0530

    complete todo-list

--- core_javascript/scripts/11-todo-list.js
+++ core_javascript/scripts/11-todo-list.js
@@ -1,4 +1,12 @@
-const todoList=[];
+const todoList=[
+    {
+    name:'make dinner ',
+    dueDate:'2022-12-25'
+},
+{
+    name:'waash clothes',
+    dueDate:'2022-12-43'
+}];
 
 //renderTodoList();
 
@@ -6,8 +14,21 @@ function renderTodoList(){
     let todoListHTML='';
 
     for(let i=0;i<todoList.length;i++){
-        const todo=todoList[i];
-        const html=`<p>${todo}</p>`;
+        const todoObject=todoList[i];
+        //const name=todoObject.name;
+        //const dueDate=todoObject.dueDate;
+        const{name,dueDate}=todoObject;
+        
+       const html = `
+  <div class="todo-grid">
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button onclick="
+      todoList.splice(${i}, 1);
+      renderTodoList();
+    " class="delete">DELETE</button>
+  </div>
+`;
         todoListHTML+=html;
     }
 
@@ -16,17 +37,16 @@ function renderTodoList(){
 
 function addTodo(){
     const inputElement=document.querySelector('.js-name-input');
+    const dateinputElement=document.querySelector('.js-due-date-input');
 
     const name=inputElement.value;
+    const dueDate=dateinputElement.value;
+    
 
-    console.log(name);
-
-    todoList.push(name);
-    console.log(todoList);
-
-    // document.querySelector('.js-todo-list').innerHTML=`${todoList}`;
+    todoList.push({ name,dueDate });
 
     inputElement.value='';
+    dateinputElement.value='';
     renderTodoList();
 }
 
